f4a1edbeb35832a1d66a48e3938c3297e3f65b88
gazakorompis <grkorompis@gmail.com>
2023-12-26 01:00:11 +0700

    feat: add wrapper rbac to restrict put, del  routes in tweets

--- server/routes/tweets/index.py
+++ server/routes/tweets/index.py
@@ -7,16 +7,17 @@ from controller.tweets.delete import delete_params_tweet
 
 from wrapper.protect_route import protect_route
 from wrapper.permit_role import permit_role_custom
+from wrapper.allow_modify_fields import allow_modify_fields
 
 tweets_blueprint = Blueprint("tweets", __name__)
 @tweets_blueprint.route("/", methods=["GET"])
 @protect_route
-@permit_role_custom(["admin", "member"], "createdBy")
+# @permit_role_custom(["admin", "member"], "createdBy")
 def get_route(wrapper_data):
     try:
         dict_query = request.args.to_dict()
         tweet_id = dict_query.get('_id')
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         print(">>> restrict query at get tweet route", restrict_query)
         if tweet_id:
             return get_one_tweets(tweet_id) #one query
@@ -35,7 +36,7 @@ def post_route(wrapper_data):
         if not body:
             errorMessage = {"message": "bad request. Body is expected"}
             return jsonify(errorMessage), 400
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         print(">>>restrict query at post route", restrict_query)
         return post_tweet(restrict_query)
     except Exception as e:
@@ -47,11 +48,10 @@ def post_route(wrapper_data):
 tweets_params_blueprint = Blueprint("tweets_one", __name__)
 @tweets_params_blueprint.route("/", methods=["GET"])
 @protect_route
-@permit_role_custom(["admin", "member"], "createdBy")
 def get_params_route(wrapper_data):
     try:
         id = wrapper_data.get("id")
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         return get_params_tweet(id, restrict_query)
     except Exception as e:
         errorMessage = {"message": str(e), "script": f"error at, {get_params_route.__name__}!"}
@@ -60,10 +60,11 @@ def get_params_route(wrapper_data):
 @tweets_params_blueprint.route("/", methods=["PUT"])
 @protect_route
 @permit_role_custom(["admin", "member"], "createdBy")
+@allow_modify_fields(["tweet", "comment"])
 def put_params_route(wrapper_data):
     try:
         id = wrapper_data.get("id")
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         return put_params_tweet(id, restrict_query)
     except Exception as e:
         errorMessage = {"message": str(e), "script": f"error at, {put_params_route.__name__}!"}
@@ -75,7 +76,7 @@ def put_params_route(wrapper_data):
 def delete_params_route(wrapper_data):
     try:
         id = wrapper_data.get("id")
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         return delete_params_tweet(id, restrict_query)
     except Exception as e:
         errorMessage = {"message": str(e), "script": f"error at, {delete_params_route.__name__}!"}
@@ -89,7 +90,7 @@ tweets_aggregator_params_blueprint = Blueprint("tweets_aggregator", __name__)
 def pull_item_route(wrapper_data):
     try:
         id = wrapper_data.get("id")
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         return pull_item_tweet(id, restrict_query)
     except Exception as e:
         errorMessage = {"message": str(e), "script": f"error at, {pull_item_route.__name__}!"}
@@ -102,7 +103,7 @@ def pull_item_route(wrapper_data):
 def apppend_item_route(wrapper_data):
     try:
         id = wrapper_data.get("id")
-        restrict_query = wrapper_data.get("restrict_query")
+        restrict_query = wrapper_data.get("restrict_query") if wrapper_data.get("restrict_query") else {}
         return append_item_tweet(id, restrict_query)
     except Exception as e:
         errorMessage = {"message": str(e),"script": f"error at, {apppend_item_route.__name__}!"}
